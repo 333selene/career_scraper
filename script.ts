7e0dbd6b52054a0ebf9a41daee6f1f3a7f6fa6e1
333selene <3.selenium@gmail.com>
2025-10-16 05:44:03 -0400

    need ot fix job description and reqs

--- script.ts
+++ script.ts
@@ -11,6 +11,17 @@ interface JobData {
   jobRequirements: string[];
 }
 
+interface JobFields {
+  jobCategory: string;
+  jobCountry: string;
+  jobCity: string;
+}
+
+interface JobText {
+  jobDescription: string[];
+  jobRequirements: string[];
+}
+
 const firefox = playwright.firefox;
 
 async function getJobPages(url: string, page: Browser.Page): Promise<string[]> {
@@ -30,22 +41,85 @@ async function getHtmlFromJobPage(
   siteLocation: string,
   page: Browser.Page,
 ): Promise<string> {
-  const url = baseUrl + siteLocation;
+  const url = `${baseUrl}${siteLocation}`;
+  console.log(url);
   await page.goto(url);
   const htmlContent = await page.content();
   return htmlContent;
 }
 
+function parseJobTitle($: Cheerio.CheerioAPI): string {
+  return $("h1.page-header").text() ?? "";
+}
+
+function parseJobFields($: Cheerio.CheerioAPI): JobFields {
+  let jobCategory: string = "";
+  let jobCountry: string = "";
+  let jobCity: string = "";
+
+  $(".job-view-fields tr").each((_, element) => {
+    const $tr = $(element);
+    const label = $tr.find("td.job-field").text().trim();
+    if (label && label === "Category") {
+      const val = $tr.find("td.job-field-value").text().trim();
+      jobCategory = val ?? "";
+    }
+    if (label && label === "Location") {
+      const val = $tr.find("td.job-field-value").text().trim();
+      jobCountry = val ?? "";
+    }
+    if (label && label === "City") {
+      const val = $tr.find("td.job-field-value").text().trim();
+      jobCity = val ?? "";
+    }
+  });
+  return { jobCategory, jobCountry, jobCity };
+}
+
+function parseJobText(html: string): JobText {
+  const jobDescription: string[] = [];
+  const jobRequirements: string[] = [];
+  const regex_desc = new RegExp(
+    "/<strong>What you'll be doing\X*.*?(?=<strong>What you'll need:)",
+  );
+  const regex_req = new RegExp(
+    "<strong>What you'll need\X*.?(?=<strong>Who you are)",
+  );
+  const html_desc = regex_desc.exec(html);
+  const html_req = regex_req.exec(html);
+  if (html_desc) {
+    const desc = html_desc[0];
+    const $desc = Cheerio.load(desc);
+    $desc("li").each((_, element) => {
+      const text = $desc(element).text().trim();
+      jobDescription.push(text);
+    });
+  }
+  if (html_req) {
+    const req = html_req[0];
+    const $desc = Cheerio.load(req);
+    $desc("li").each((_, element) => {
+      const text = $desc(element).text().trim();
+      jobRequirements.push(text);
+    });
+  }
+  return { jobDescription, jobRequirements };
+}
+
 function getJobDataFromHtml(html: string): JobData {
-  const temp: JobData = {
-    jobTitle: "",
-    jobCategory: "",
-    jobCountry: "",
-    jobCity: "",
-    jobDescription: [],
-    jobRequirements: [],
+  const $ = Cheerio.load(html);
+  const jobTitle = parseJobTitle($);
+  const { jobCountry, jobCategory, jobCity } = parseJobFields($);
+  const { jobDescription, jobRequirements } = parseJobText(html);
+  const data: JobData = {
+    jobTitle,
+    jobCategory,
+    jobCity,
+    jobCountry,
+    jobDescription,
+    jobRequirements,
   };
-  return temp;
+  return data;
 }
 
 async function main(): Promise<void> {
@@ -60,21 +134,21 @@ async function main(): Promise<void> {
     console.log("job urls:");
     console.log(results);
     const htmls: string[] = [];
-    results.forEach((siteLocation) => async () => {
+    for (const siteLocation of results) {
       const content = await getHtmlFromJobPage(
         "https://wpp.careersitecloud.com",
         siteLocation,
         page,
       );
+
       htmls.push(content);
-    });
+    }
     const jobs: JobData[] = [];
     htmls.forEach((content) => {
       const info = getJobDataFromHtml(content);
       jobs.push(info);
     });
     console.log(jobs);
-    console.log("finished");
   } finally {
     await browser.instance.close();
   }
