50ac0e2e2bec83e0ffde3d2a0536c5ed191b82bc
333selene <3.selenium@gmail.com>
2025-10-16 04:09:24 -0400

    program scaffold

--- script.ts
+++ script.ts
@@ -1,31 +1,83 @@
 import playwright = require("playwright");
 import Cheerio = require("cheerio");
+import type Browser = require("playwright");
+
+interface JobData {
+  jobTitle: string;
+  jobCategory: string;
+  jobCountry: string;
+  jobCity: string;
+  jobDescription: string[];
+  jobRequirements: string[];
+}
 
 const firefox = playwright.firefox;
 
-async function scrapePage(url: string): Promise<string[]> {
-  const browser = { name: "firefox", instance: await firefox.launch() };
-
-  const page = await browser.instance.newPage();
+async function getJobPages(url: string, page: Browser.Page): Promise<string[]> {
   let results: string[] = [];
-  try {
-    await page.goto(url);
-    const htmlContent = await page.content();
-    console.log(htmlContent);
-    const $ = Cheerio.load(htmlContent);
-    $(".job-listing-full a").each((_, element) => {
-      const link = $(element).attr("href");
-      link && results.push(link);
-    });
-  } finally {
-    await browser.instance.close();
-  }
+  await page.goto(url);
+  const htmlContent = await page.content();
+  const $ = Cheerio.load(htmlContent);
+  $(".job-listing-full a").each((_, element) => {
+    const link = $(element).attr("href");
+    link && results.push(link);
+  });
   return results;
 }
 
-scrapePage("https://wpp.careersitecloud.com/search")
-  .then((results) => {
-    console.log("scraping...");
+async function getHtmlFromJobPage(
+  baseUrl: string,
+  siteLocation: string,
+  page: Browser.Page,
+): Promise<string> {
+  const url = baseUrl + siteLocation;
+  await page.goto(url);
+  const htmlContent = await page.content();
+  return htmlContent;
+}
+
+function getJobDataFromHtml(html: string): JobData {
+  const temp: JobData = {
+    jobTitle: "",
+    jobCategory: "",
+    jobCountry: "",
+    jobCity: "",
+    jobDescription: [],
+    jobRequirements: [],
+  };
+  return temp;
+}
+
+async function main(): Promise<void> {
+  console.log("scraping...");
+  const browser = { name: "firefox", instance: await firefox.launch() };
+  try {
+    const page = await browser.instance.newPage();
+    const results = await getJobPages(
+      "https://wpp.careersitecloud.com/search",
+      page,
+    );
+    console.log("job urls:");
     console.log(results);
-  })
-  .catch(console.error);
+    const htmls: string[] = [];
+    results.forEach((siteLocation) => async () => {
+      const content = await getHtmlFromJobPage(
+        "https://wpp.careersitecloud.com",
+        siteLocation,
+        page,
+      );
+      htmls.push(content);
+    });
+    const jobs: JobData[] = [];
+    htmls.forEach((content) => {
+      const info = getJobDataFromHtml(content);
+      jobs.push(info);
+    });
+    console.log(jobs);
+    console.log("finished");
+  } finally {
+    await browser.instance.close();
+  }
+}
+
+main().then().catch(console.error);
