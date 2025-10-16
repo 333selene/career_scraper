1f27a3344316b1179237d5129ea0ef3585cdd787
333selene <3.selenium@gmail.com>
2025-10-16 06:53:19 -0400

    cli framework

--- script.ts
+++ script.ts
@@ -1,6 +1,19 @@
-import playwright = require("playwright");
-import Cheerio = require("cheerio");
+#!/usr/bin/env node
+import { Command } from "commander";
+import { firefox } from "playwright";
+import * as Cheerio from "cheerio";
 import type Browser = require("playwright");
+
+const program = new Command();
+program
+  .version("1.0.0")
+  .description("example webscraper")
+  .option("-u, --url  [string]", "url to get job listings from")
+  .option("-b, --base [string]", "base url to append job listing links")
+  .option("-o, --output [file]", "output file")
+  .parse(process.argv);
+
+const options = program.opts();
 
 interface JobData {
   jobTitle: string;
@@ -15,8 +28,6 @@ interface JobFields {
   jobCountry: string;
   jobCity: string;
 }
-
-const firefox = playwright.firefox;
 
 async function getJobPages(url: string, page: Browser.Page): Promise<string[]> {
   let results: string[] = [];
@@ -90,23 +101,32 @@ function getJobDataFromHtml(html: string): JobData {
 }
 
 async function main(): Promise<void> {
-  console.log("scraping...");
+  let jobPagesUrl = "";
+  let jobsBaseUrl = "";
+  let outputFile = "output.json";
+
+  if (options.url && typeof options.url === "string") {
+    jobPagesUrl = options.url;
+  } else {
+    throw new Error("You must provide a url with job listings");
+  }
+
+  if (options.base && typeof options.base === "string") {
+    jobsBaseUrl = options.base;
+  } else {
+    throw new Error("You must provide a base url shared by all job listings");
+  }
+  console.log("Starting...");
   const browser = { name: "firefox", instance: await firefox.launch() };
   try {
     const page = await browser.instance.newPage();
-    const results = await getJobPages(
-      "https://wpp.careersitecloud.com/search",
-      page,
-    );
+
+    const results = await getJobPages(jobPagesUrl, page);
     console.log("job urls:");
     console.log(results);
     const htmls: string[] = [];
     for (const siteLocation of results) {
-      const content = await getHtmlFromJobPage(
-        "https://wpp.careersitecloud.com",
-        siteLocation,
-        page,
-      );
+      const content = await getHtmlFromJobPage(jobsBaseUrl, siteLocation, page);
 
       htmls.push(content);
     }
